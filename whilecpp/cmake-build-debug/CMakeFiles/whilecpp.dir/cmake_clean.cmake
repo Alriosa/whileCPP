2b5a8ae7d0f5cb39ca256204705984a30eb82186
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-02-13 14:42:57 -0600

    1

---
+++ whilecpp/cmake-build-debug/CMakeFiles/whilecpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/whilecpp.dir/main.cpp.o"
+  "whilecpp.pdb"
+  "whilecpp.exe"
+  "libwhilecpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/whilecpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ whilecpp/main.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+    int max = 5;
+    int number =1;
+
+
+    while (number<max){
+        cout<<"The number right now is: "<<number<<endl;
+        number++;
+    }
+
+    cout<<"The final number is: "<<number;
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
